f12a02dc4b20237dd5038763b65cbf9be504463d
Saxon Jensen <saxon.jensen@gmail.com>
2014-05-14 16:51:19 +0800

    Added some model validation

--- Knowts/app/models/item.rb
+++ Knowts/app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ActiveRecord::Base
   has_and_belongs_to_many :users
   belongs_to :list
+
+  validates :due, presence: true
+  validates :content, presence: true, length: {within: 3..150}
 end
--- Knowts/app/models/user.rb
+++ Knowts/app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 
   has_and_belongs_to_many :workspaces
   has_and_belongs_to_many :items
+
+  validates :name, presence: true, length: {minimum: 3}
 end
