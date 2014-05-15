1e84bc6a409b0f062800a61bb0e5f9bb6b74f9cd
Saxon Jensen <saxon.jensen@gmail.com>
2014-05-15 22:28:22 +0800

    Login page implemented. Login page now logs in the user correctly. Fixed relative paths to fix errors when cancelling a login.

--- Knowts/config/routes.rb
+++ Knowts/config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   get '/index' => 'home#index'
   get '/about' => 'home#about'
   get '/sitemap' => 'home#sitemap'
+  get '/login' => 'accounts#sign_in'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
