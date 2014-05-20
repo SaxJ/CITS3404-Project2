f5426b54db11e1422d7c6de924014eeb4bbb877f
Saxon Jensen <saxon.jensen@gmail.com>
2014-05-20 21:45:10 +0800

    Editing a workspace looks way better now. And adding users to a workspace works a lot better.

--- Knowts/app/assets/javascripts/workspaces.js
+++ Knowts/app/assets/javascripts/workspaces.js
@@ -3,10 +3,21 @@ $('.show_users_btn').click(function() {
     if (!btn.hasClass('active')) {
         btn.addClass('active');
         $('.users').addClass('shown');
-        btn.innerHTML = 'Hide Users';
+        btn.text('Hide Users');
     } else {
         btn.removeClass('active');
         $('.users').removeClass('shown');
-        btn.innerHTML = 'Show Users';
+        btn.text('Show Users');
     }
-})
+});
+
+
+$('#add_workspace_user_button').click(function(e) {
+    if ($('#add_workspace_user_button').hasClass('expanded') && !$(e.target).hasClass('close')) {
+        return;
+    }
+    $('#add_workspace_user_button .close').toggleClass('shown');
+    $('#add_workspace_user_button .plus').toggleClass('shown');
+    $('#add_workspace_user_button form').toggleClass('shown');
+    $('#add_workspace_user_button').toggleClass('expanded');
+});
