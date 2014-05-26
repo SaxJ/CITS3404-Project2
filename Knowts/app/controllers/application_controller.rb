3f8c41632cef78ba62765403ec94351875cdfd27
Saxon Jensen <saxon.jensen@gmail.com>
2014-05-26 23:00:29 +0800

    Will now redirect to the '/app' page after logging in.

--- Knowts/app/controllers/application_controller.rb
+++ Knowts/app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :name, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
   end
+
+  def after_sign_in_path_for(resource)
+    return '/app'
+  end
 end
